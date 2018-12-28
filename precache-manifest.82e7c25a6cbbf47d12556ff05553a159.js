4e9ca48cf9d53b74b5e9b40b5d341ad844efe934
minooo <x13603983223@163.com>
2018-12-28 17:31:53 +0800

    Updates

---
+++ precache-manifest.82e7c25a6cbbf47d12556ff05553a159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minooo-blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd479761dd0f85c00c38",
+    "url": "/minooo-blog/static/js/runtime~main.cd479761.js"
+  },
+  {
+    "revision": "7108e0eedd01f8cb59cc",
+    "url": "/minooo-blog/static/js/main.7108e0ee.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/minooo-blog/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7108e0eedd01f8cb59cc",
+    "url": "/minooo-blog/static/css/main.bd8ad5a4.chunk.css"
+  },
+  {
+    "revision": "b0bd27fb809a355a2f37629f7f4ced1d",
+    "url": "/minooo-blog/index.html"
+  }
+];
