566359f1ea5c72fcf8db10ca830726b7acbcf501
minooo <x13603983223@163.com>
2019-01-02 09:23:38 +0800

    'base'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React321
-          </a>
-        </header>
+      <div dangerouslySetInnerHTML={{__html: `<iframe height='300' scrolling='no' title='Gradient button.' src='//codepen.io/spk123/embed/qLXjov/?height=300&theme-id=25924&default-tab=css,result' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'>See the Pen <a href='https://codepen.io/spk123/pen/qLXjov/'>Gradient button.</a> by sachin (<a href='https://codepen.io/spk123'>@spk123</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>`}}>
+        
       </div>
     );
   }
